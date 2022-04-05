3dfe4a5d5ba95172c3deac7aef301bf2c8c54c0b
Francisco Macedo <francisco.macedo@optimile.eu>
2022-04-05 22:40:49 +0100

    update master to main in deploy script

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -3,7 +3,6 @@ const execa = require("execa");
 var rimraf = require("rimraf");
 const fs = require("fs");
 
-
 (async () => {
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
@@ -16,8 +15,10 @@ const fs = require("fs");
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    await rimraf(folderName, function () { console.log("removed dist folder"); });
-    await execa("git", ["checkout", "-f", "master"]);
+    await rimraf(folderName, function () {
+      console.log("removed dist folder");
+    });
+    await execa("git", ["checkout", "-f", "main"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed");
   } catch (e) {
